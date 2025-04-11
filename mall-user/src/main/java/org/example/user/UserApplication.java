3fd742fae8f148135fce93b756801bc897332607
LunKeee <xl755377063@outlook.com>
2025-04-12 01:40:34 +0800

    test "org/example/common/config/SwaggerConfig.class"

--- mall-user/src/main/java/org/example/user/UserApplication.java
+++ mall-user/src/main/java/org/example/user/UserApplication.java
@@ -9,5 +9,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UserApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
+
+//        System.out.println("Hello UserApplication");
+
+        System.out.println(ClassLoader.getSystemClassLoader().getResource("org/example/common/config/SwaggerConfig.class"));
+
     }
 }
